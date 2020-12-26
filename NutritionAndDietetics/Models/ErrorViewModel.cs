2e06393839329bb701351e0da64e57d2c3e8769a
Mevlüt <mwltgr@gmail.com>
2020-12-26 22:07:09 +0300

    Add project files.

---
+++ NutritionAndDietetics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NutritionAndDietetics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
